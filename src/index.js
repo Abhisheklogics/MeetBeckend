1ca4544a73b8d958ef681717624c4bdfc8657496
abhi.Logics <abhi.kumar252004@gmail.com>
2025-07-14 01:12:14 +0530

    ak

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import http from 'http';
 import express from 'express';
 import { Server } from 'socket.io';
 import cors from 'cors';
-a
+
 const app = express().use(cors());
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: 'https://conexus-iota.vercel.app/', credentials: true } });
